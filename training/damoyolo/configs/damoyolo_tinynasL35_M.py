ba703bb24e19ea4c17caa45a56edf37bc8519400
Masahiro Kisono <mkisono@gmail.com>
2025-12-16 06:17:33 +0900

    Update training configuration parameters in damoyolo_tinynasL35_M.py for improved performance

--- training/damoyolo/configs/damoyolo_tinynasL35_M.py
+++ training/damoyolo/configs/damoyolo_tinynasL35_M.py
@@ -10,26 +10,26 @@ class Config(MyConfig):
         super(Config, self).__init__()
 
         self.miscs.exp_name = datetime.now().strftime("%Y%m%d_%H%M%S")
-        self.miscs.eval_interval_epochs = 10
-        self.miscs.ckpt_interval_epochs = 10
+        self.miscs.eval_interval_epochs = 5
+        self.miscs.ckpt_interval_epochs = 5
 
         self.train.total_epochs = 100
         self.train.finetune_path = "damoyolo_tinynasL35_M_487.pth"
 
         # optimizer
         self.train.batch_size = 16
-        self.train.base_lr_per_img = 0.01 / 64
-        self.train.min_lr_ratio = 0.05
+        self.train.base_lr_per_img = 0.015 / 64
+        self.train.min_lr_ratio = 0.01
         self.train.weight_decay = 5e-4
         self.train.momentum = 0.9
-        self.train.no_aug_epochs = 16
-        self.train.warmup_epochs = 5
+        self.train.no_aug_epochs = 20
+        self.train.warmup_epochs = 10
 
         # augment
         self.train.augment.transform.image_max_range = (640, 640)
-        self.train.augment.mosaic_mixup.mixup_prob = 0.15
-        self.train.augment.mosaic_mixup.degrees = 10.0
-        self.train.augment.mosaic_mixup.translate = 0.2
+        self.train.augment.mosaic_mixup.mixup_prob = 0.5
+        self.train.augment.mosaic_mixup.degrees = 15.0
+        self.train.augment.mosaic_mixup.translate = 0.3
         self.train.augment.mosaic_mixup.shear = 2.0
         self.train.augment.mosaic_mixup.mosaic_scale = (0.1, 2.0)
 
